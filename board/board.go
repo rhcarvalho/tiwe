47525f1cfaa2d2a813e28ef04141dcb9f08b59ee
Rodolfo Carvalho <rhcarvalho@gmail.com>
2018-03-03 23:12:30 -0800

    Define sets as containing runs and/or groups

--- board/board.go
+++ board/board.go
@@ -3,7 +3,60 @@ package board
 type Board [][]Tile
 
 func (b Board) Valid() bool {
+	for _, set := range b {
+		if !isRun(set) && !isGroup(set) {
+			return false
+		}
+	}
 	return true
 }
 
-type Tile uint64
+func isRun(set []Tile) bool {
+	if len(set) < 3 {
+		return false
+	}
+	for i := 0; i < len(set)-1; i++ {
+		if set[i+1].Value-set[i].Value != 1 {
+			return false
+		}
+		if set[i].Color != set[i+1].Color {
+			return false
+		}
+	}
+	return true
+}
+
+func isGroup(set []Tile) bool {
+	if len(set) < 3 {
+		return false
+	}
+	var seen int64
+	for i := 0; i < len(set)-1; i++ {
+		if set[i].Value != set[i+1].Value {
+			return false
+		}
+		seen |= 1 << set[i].Color
+		if (seen>>set[i+1].Color)&1 == 1 {
+			return false
+		}
+	}
+	return true
+}
+
+func (b Board) Add(ts ...Tile) Board {
+	return append(b, ts)
+}
+
+type Tile struct {
+	Value uint64
+	Color
+}
+
+type Color uint64
+
+const (
+	Red Color = iota
+	Green
+	Blue
+	Yellow
+)
--- board/board_test.go
+++ board/board_test.go
@@ -13,6 +13,33 @@ func TestBoardValid(t *testing.T) {
 			board: Board{},
 			valid: true,
 		},
+		{
+			name: "sets must contain at least 3 tiles",
+			board: Board{}.
+				Add(Tile{2, Red}, Tile{3, Red}).
+				Add(Tile{7, Red}, Tile{7, Green}),
+			valid: false,
+		},
+		{
+			name:  "runs must be contiguous",
+			board: Board{}.Add(Tile{2, Red}, Tile{3, Red}, Tile{5, Red}),
+			valid: false,
+		},
+		{
+			name:  "runs must use tiles of the same color",
+			board: Board{}.Add(Tile{2, Red}, Tile{3, Red}, Tile{4, Green}),
+			valid: false,
+		},
+		{
+			name:  "sets can contain groups",
+			board: Board{}.Add(Tile{7, Red}, Tile{7, Green}, Tile{7, Blue}),
+			valid: true,
+		},
+		{
+			name:  "groups must not contain repeated colors",
+			board: Board{}.Add(Tile{7, Red}, Tile{7, Green}, Tile{7, Red}),
+			valid: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
